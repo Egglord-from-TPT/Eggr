ecf9bfafe9ca49e5abe0fc78a2fc7e890137283c
Egglord-from-TPT <karlamongus6767@gmail.com>
2026-03-02 00:46:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="eggr",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(),
     description="A Python package that extracts the longest reachable increasing sequence from a list of numbers where each step cannot exceed a specified distance.",
     author="",
